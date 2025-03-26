98a45dec91fa0407db02aebc3b0b9ba5e4ff826d
harvey puyong <harveyluslos80@gmail.com>
2025-03-26 21:46:41 +0800

    Add phone-view styles to goals-section

--- src/components/GoalsCard.jsx
+++ src/components/GoalsCard.jsx
@@ -1,8 +1,10 @@
 function GoalCard({data}){
   return(
-    <div className="goal-card">
-      <img src={data.icon} alt="" className="goal-icon" />
-      <p className="goal-label">{data.label}</p>
+    <div className="card">
+      <div className="icon-container">
+        <img src={data.icon} alt="" className="icon" />
+      </div>
+      <p className="label">{data.label}</p>
     </div>
   )
 }
--- src/sections/GoalsSection.jsx
+++ src/sections/GoalsSection.jsx
@@ -3,6 +3,7 @@ import HealthyPigIcon from "./../assets/icons/healthy-pig.png"
 import CommunicationIcon from "./../assets/icons/seamless-communication.png"
 import ProfitableIcon from "./../assets/icons/profitable.png"
 import GoalCard from "../components/GoalsCard"
+import "./../styles/sectionsStyle/goalsSection.css"
 
 
 const goalsCardData = [
@@ -25,16 +26,15 @@ const goalsCardData = [
 function GoalsSection(){
   return(
     <section id="goals-section">
-      <div className="img-info-container">
-        <div className="images">
-          <img src={PigsImg} alt="" />
-        </div>
+      <div className="green-background"></div>
+      <div className="img-info-container d-flex">
+        <img className="pig-image" src={PigsImg} alt={PigsImg} />
         <div className="info">
-          <h1 className="heading">Our Goal: 
+          <h1 className="heading">Our Goal: <br />
             <span className="sub-heading">Revolutionizing Swine Farming with Modern Technology</span>
           </h1>
           <p className="details">
-            We are committed to promoting healthy and sustainable swine farming by fostering transparency between local swine raisers and the provincial veterinary office. Our platform serves as a bridge of information, empowering farmers with real-time insights and expert guidance.
+            &nbsp;&nbsp;We are committed to promoting healthy and sustainable swine farming by fostering transparency between local swine raisers and the provincial veterinary office. Our platform serves as a bridge of information, empowering farmers with real-time insights and expert guidance.
             By harnessing modern technology, we strive to enhance efficiency, productivity, and profitability in the swine industry. Together, we are shaping a smarter, more connected, and more profitable future for swine farming.
           </p>
         </div>
